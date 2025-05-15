ccfa26632bff86fbd27b3c237b6557f34b6d07d6
lanceadrian21 <lancepaladin91@gmail.com>
2025-05-16 02:33:57 +0800

    redo

--- src/firebase.js
+++ src/firebase.js
@@ -5,20 +5,21 @@ import {
   browserLocalPersistence
 } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDI0byWWUJNSqfOQfEONvE-A8EDp18kiQU",
-  authDomain: "task-management-4d66e.firebaseapp.com",
-  projectId: "task-management-4d66e",
-  storageBucket: "task-management-4d66e.appspot.com",
-  messagingSenderId: "107888627926",
-  appId: "1:107888627926:web:64ea640c0085a6382ff977",
-  measurementId: "G-C1Z78Q88KJ",
-  databaseURL: "https://task-management-4d66e-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  apiKey: "AIzaSyBJr0TmUn3oFvG9dV-eW-WPV9FCquHELjU",
+  authDomain: "taskmanagement-792d6.firebaseapp.com",
+  projectId: "taskmanagement-792d6",
+  storageBucket: "taskmanagement-792d6.firebasestorage.app",
+  messagingSenderId: "778942239972",
+  appId: "1:778942239972:web:dbd3c3f65917c76afe08f1",
+  measurementId: "G-7EJMLMS8TY",
+  databaseURL: "https://taskmanagement-792d6-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
-
+const analytics = getAnalytics(app);
 const auth = getAuth(app);
 setPersistence(auth, browserLocalPersistence); // ✅ Persist login across sessions
 
